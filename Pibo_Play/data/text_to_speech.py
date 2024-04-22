4cd506940746d159638374194db4269d6bfb632d
daaa02 <dyk98498@gmail.com>
2024-04-22 17:32:50 +0900

    change CLOVA

--- Pibo_Play/data/text_to_speech.py
+++ Pibo_Play/data/text_to_speech.py
@@ -21,8 +21,8 @@ class TextToSpeech():
     
     def tts_connection(self, voice, text, filename):
         # CLOVA auth-key
-        client_id = "3qz5jqx2r0"
-        client_secret = "zwB0Yb4UONPKaOKCjZkhsSl8REuKvJTYK2Esvr41"
+        client_id = "9rx70l2455"
+        client_secret = "zC7febcAzgI4oTQ25DODSqDKerk0lUwhwIAbVvsA"
         encText = urllib.parse.quote(text)
         data = f"speaker={voice}&volume=0&speed=1&pitch=0&format=wav&text=" + encText
         url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
